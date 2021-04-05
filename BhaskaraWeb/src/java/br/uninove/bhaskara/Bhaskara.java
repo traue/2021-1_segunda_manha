30e2446157d064629392e301abebb97eb691e85d
Thiago G. Traue <t.traue@gmail.com>
2021-04-05 11:31:10 -0300

    Update Bhaskara.java

--- BhaskaraWeb/src/java/br/uninove/bhaskara/Bhaskara.java
+++ BhaskaraWeb/src/java/br/uninove/bhaskara/Bhaskara.java
@@ -7,3 +7,11 @@ public class Bhaskara {
     
     //implementar toda a lógica...
 }
+
+
+/*
+    Delta = (b*b) - 4 * a * c
+    o delta não pode ser negativo para calcular as raizes
+    raizPositiva = (-b + raiz(delta)) / 2 * a
+    raizNegativa = (-b - raiz(delta)) / 2 * a
+*/
