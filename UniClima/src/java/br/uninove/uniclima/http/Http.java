e3d12ba57bb14ce6c0c5baf9f75e2f0fc347247c
Thiago G. Traue <t.traue@gmail.com>
2021-04-26 11:26:17 -0300

    Continuação do projeto

--- UniClima/src/java/br/uninove/uniclima/http/Http.java
+++ UniClima/src/java/br/uninove/uniclima/http/Http.java
@@ -1,49 +1,61 @@
 package br.uninove.uniclima.http;
 
 import br.uninove.uniclima.tempo.Tempo;
+import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
-
 public class Http {
-    
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
     public static Tempo getClima(String cidade) {
         try {
-            String url = "http://api.openweathermap.org/data/2.5/weather";
             String charset = StandardCharsets.UTF_8.name();
-            String appid = ""; //COLOCAR O SEU API KEY
+            String url = "http://api.openweathermap.org/data/2.5/weather";
+            String appid = "XXXXXX"; //COLOCAR O SEU API KEY
             String units = "metric";
             String lang = "pt_br";
-            
+
             String query = String.format("q=%s&appid=%s&units=%s&lang=%s",
-                       URLEncoder.encode(cidade, charset),
-                       URLEncoder.encode(appid, charset),
-                       URLEncoder.encode(units, charset),
-                       URLEncoder.encode(lang, charset));
-            
+                    URLEncoder.encode(cidade, charset),
+                    URLEncoder.encode(appid, charset),
+                    URLEncoder.encode(units, charset),
+                    URLEncoder.encode(lang, charset));
+
             URLConnection conn = new URL(url + "?" + query).openConnection();
-            
+
             conn.setRequestProperty("Accept-Charset", charset);
             conn.setDoOutput(true);
             conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=" + charset);
             Tempo t;
-            try(InputStream resp = conn.getInputStream()) {
+            try (InputStream resp = conn.getInputStream()) {
                 BufferedReader buffer = new BufferedReader(new InputStreamReader(resp, Charset.forName("UTF-8")));
-                //paramos aqui...
-                
+                String json = readAll(buffer);
+                Gson gson = new Gson();
+                t = gson.fromJson(json, Tempo.class);
             }
-            
-        } catch(JsonSyntaxException | IOException e) {
-            
+            return t;
+        } catch (JsonSyntaxException | IOException e) {
+            e.printStackTrace();
         }
+
         return null;
     }
 }
---
+++ UniClima/src/java/br/uninove/uniclima/util/StrUtils.java
@@ -0,0 +1,27 @@
+package br.uninove.uniclima.util;
+
+public class StrUtils {
+    
+    public static String convertToTitleCaseIteratingChars(String text) {
+        if (text == null || text.isEmpty()) {
+            return text;
+        }
+
+        StringBuilder converted = new StringBuilder();
+
+        boolean convertNext = true;
+        for (char ch : text.toCharArray()) {
+            if (Character.isSpaceChar(ch)) {
+                convertNext = true;
+            } else if (convertNext) {
+                ch = Character.toTitleCase(ch);
+                convertNext = false;
+            } else {
+                ch = Character.toLowerCase(ch);
+            }
+            converted.append(ch);
+        }
+
+        return converted.toString();
+    }
+}
