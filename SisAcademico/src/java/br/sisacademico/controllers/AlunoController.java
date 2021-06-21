c55c07dc3d1e13e4737e0760b79d877e4a3fb33e
Thiago G. Traue <t.traue@gmail.com>
2021-06-21 10:27:41 -0300

    Correção

--- SisAcademico/src/java/br/sisacademico/controllers/AlunoController.java
+++ SisAcademico/src/java/br/sisacademico/controllers/AlunoController.java
@@ -15,6 +15,7 @@ public class AlunoController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
         try (PrintWriter out = response.getWriter()) {
 
             String acao = request.getParameter("acao");
